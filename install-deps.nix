f35a8698e2a45b45b65c4cf355ff8b3044327f39
justinwoo <moomoowoo@gmail.com>
2018-12-06 16:25:07 +0200

    update psc-package2nix usage

--- install-deps.nix
+++ install-deps.nix
@@ -1,16 +1,9 @@
 let
   pkgs = import <nixpkgs> {};
   packages = import ./packages.nix {};
-  packageDrvs = builtins.attrValues packages.inputs;
-  pp2n-utils = import (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/justinwoo/psc-package2nix/409aab26afa0784eb90440da33b1ad4d56aedb93/utils.nix";
-    sha256 = "0rkqisfvpz5x8j2p0llv0yzgz5vnzy7fcfalp8nkymbshk8702gg";
+  pp2n-utils =  import (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/justinwoo/psc-package2nix/0ab7fbb6e55af4cc224ddbcfcf62af3f29a3f363/utils.nix";
+    sha256 = "1hw7dyxhd0hhfx38fhhgh9xj53ki3m2682bv2igashiakxvhcclb";
   });
 
-in pkgs.stdenv.mkDerivation {
-  name = "install-deps";
-
-  buildInputs = packageDrvs;
-
-  shellHook = pp2n-utils.mkDefaultShellHook packages packageDrvs;
-}
+in pp2n-utils.mkInstallPackages pkgs packages
